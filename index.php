53168de738c06b2d357a19b95d0a2993cf3bb7f9
Malikabhc <malika.bouhcine@gmail.com>
2018-07-26 13:43:33 +0200

    exercice7

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 7</title>
+  </head>
+  <body>
+    <?php
+      $lastname = 'B.';
+      $firstname = 'Malika';
+      $age = 26;
+      echo 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . ' ans.';
+    ?>
+  </body>
+</html>
